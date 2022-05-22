7aa3b849a0519f89f5789f08d65a559acfb578d2
Javier Ramos Nodal <javiramosnodal@gmail.com>
2022-05-22 11:21:24 +0200

    …

--- microservice-map-info/microservice-map-info/Controllers/MapInfoController.cs
+++ microservice-map-info/microservice-map-info/Controllers/MapInfoController.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GoogleMapInfo;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace microservice_map_info.Controllers
+namespace microservice_map_info.Controllers;
+
+[Route("[controller]")]
+[Route("[controller]/[action]")]
+[ApiController] public class MapInfoController : ControllerBase
 {
-    public class MapInfoController : Controller
+    private readonly GoogleDistanceApi _googleDistanceApi;
+    public MapInfoController(GoogleDistanceApi googleDistanceApi)
     {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _googleDistanceApi = googleDistanceApi;
+    }
+
+    [HttpGet]
+    public async Task<GoogleDistanceData> GetDistance(string originCity, string destinationCity)
+    {
+        return await _googleDistanceApi.GetMapDistance(originCity, destinationCity);
     }
 }
-
