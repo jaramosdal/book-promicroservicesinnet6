deff001ba3f4eb23d63c113f8d4685575a8546d9
Javier Ramos Nodal <javiramosnodal@gmail.com>
2022-06-07 22:07:16 +0200

    TestClient y PaymentMicroservice

---
+++ MessageContracts/PaymentMicroservice/InvoiceCreatedConsumer.cs
@@ -0,0 +1,14 @@
+using MassTransit;
+using MessageContracts;
+
+namespace PaymentMicroservice;
+
+
+class InvoiceCreatedConsumer : IConsumer<IInvoiceCreated>
+{
+    public async Task Consume(ConsumeContext<IInvoiceCreated> context)
+    {
+        await Task.Run(() => 
+            Console.WriteLine($"Received message for invoice number: {context.Message.InvoiceNumber}"));
+    }
+}
